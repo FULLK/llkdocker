de401073fe070d0466961dcc80244ad8157f00eb
Your Name <you@example.com>
2024-05-11 21:45:33 +0800

    overlayfs

---
+++ overlayfs_docker/container/contain_process.go
@@ -0,0 +1,35 @@
+package contain
+import(
+	log "github.com/sirupsen/logrus"
+	"os/exec"
+	"syscall"
+	"os"
+)
+
+func new_contain_process(it bool)  (*exec.Cmd,*os.File,string){	
+	
+	readpipe,writepipe,err:=os.Pipe()
+	if err!=nil{
+		log.Fatal(err)
+	}
+	command:=exec.Command("/proc/self/exe","init")
+	command.SysProcAttr=&syscall.SysProcAttr{
+		Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID | syscall.CLONE_NEWNS |
+			syscall.CLONE_NEWNET | syscall.CLONE_NEWIPC,
+	}
+	if it{
+		command.Stdin=os.Stdin
+		command.Stdout=os.Stdout
+		command.Stderr=os.Stderr
+	}
+	log.Info(command)
+	log.Infof("sucess create a contain process")
+	command.ExtraFiles=[]*os.File{readpipe}
+	mntURL := "/home/llk/Desktop/docker/src/overlayfs_docker/merged"
+	rootURL := "/home/llk/Desktop/docker/src/overlayfs_docker"
+	command.Dir = mntURL
+	prepare_overlays(rootURL) 
+	log.Info(readpipe)
+	return command,writepipe,rootURL
+	
+}
